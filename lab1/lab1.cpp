8b6fd710f77b582d36d2f3b4562984429b97d668
Drake Fisher <drake2445@gmail.com>
2016-09-01 20:52:15 +0000

    lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "-----------" << endl;    
+    cout << "|****o****|" << endl;
+    cout << "|***o*o***|" << endl;
+    cout << "|**o***o**|" << endl;
+    cout << "|***o*o***|" << endl;
+    cout << "|****o****|" << endl;
+    cout << "-----------" << endl;
+    
+    return 0;
+    
+}
